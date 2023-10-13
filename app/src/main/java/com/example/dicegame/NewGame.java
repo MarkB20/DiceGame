d3ed181041db706c3f8f485f5a8d7654d1ec482e
MarkB20 <w.markbutler2001@gmail.com>
2023-10-13 14:00:24 +0100

    added sum function for scores

--- app/src/main/java/com/example/dicegame/NewGame.java
+++ app/src/main/java/com/example/dicegame/NewGame.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.stream.IntStream;
 
 
 public class NewGame extends AppCompatActivity  {
@@ -13,13 +16,31 @@ public class NewGame extends AppCompatActivity  {
 
     boolean[] reRoll = new boolean[5];
 
+    // classes
     DiceRoller diceRoller = new DiceRoller();
 
     // creating new objects for each player
     Player P1 = new Player("Player");
     Player CPU = new Player("CPU");
 
+
+    // buttons
     Button throwButton;
+    Button scoreButton;
+
+
+    TextView roundsTxt;
+    int rounds = 0;
+
+    // player text
+    TextView playerScore;
+    TextView playerRollScore;
+    TextView P1ReRolls;
+
+    // CPU text
+    TextView CPUScore;
+    TextView CPURollScore;
+    TextView CPUReRolls;
 
 
     @Override
@@ -28,31 +49,107 @@ public class NewGame extends AppCompatActivity  {
         setContentView(R.layout.activity_new_game);
 
         throwButton = findViewById(R.id.Throw);
+        scoreButton = findViewById(R.id.Score);
 
-        // assigning each image to the image view
+        // assigning each image to the image view for P1
         P1.DiceImages[0] = findViewById(R.id.P1Dice1);
         P1.DiceImages[1] = findViewById(R.id.P1Dice2);
         P1.DiceImages[2] = findViewById(R.id.P1Dice3);
         P1.DiceImages[3] = findViewById(R.id.P1Dice4);
         P1.DiceImages[4] = findViewById(R.id.P1Dice5);
 
-
+        // assigning each image to the image view for CPU
         CPU.DiceImages[0]= findViewById(R.id.CPUDice1);
         CPU.DiceImages[1]= findViewById(R.id.CPUDice2);
         CPU.DiceImages[2]= findViewById(R.id.CPUDice3);
         CPU.DiceImages[3]= findViewById(R.id.CPUDice4);
         CPU.DiceImages[4]= findViewById(R.id.CPUDice5);
 
-
+        //displays all ones before the game starts
         diceRoller.diceInt(P1.DiceImages,CPU.DiceImages);
 
+       // assigning textview for the round score
+        roundsTxt = (TextView) findViewById(R.id.rounds);
+
+        // assigning each textview for P1
+        playerScore = (TextView) findViewById(R.id.playerScore);
+        playerRollScore = (TextView) findViewById(R.id.PlayerRollScore);
+        P1ReRolls = (TextView) findViewById(R.id.P1ReRolls);
+
+        // assigning each textview for CPU
+        CPUScore = (TextView) findViewById(R.id.CPUScore);
+        CPURollScore = (TextView) findViewById(R.id.CPURollScore);
+        CPUReRolls = (TextView) findViewById(R.id.CPUReRolls);
+
+
+        playerScore.setText(P1.name + ": " + P1.score);
+        playerRollScore.setText(P1.name + " roll score: " + P1.rollScore);
+        P1ReRolls.setText(P1.name + " Rolls: " + P1.rolls);
+
+        CPUScore.setText(CPU.name + ": " + CPU.score);
+        CPURollScore.setText(CPU.name + " roll score: " + CPU.rollScore);
+        CPUReRolls.setText(CPU.name + " Rolls: " + CPU.rolls);
+
+
+        roundsTxt.setText("round: " + rounds);
+
+
+        scoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                P1.sumOverallScores();
+                CPU.sumOverallScores();
+
+                playerScore.setText(P1.name + ": " + P1.score);
+                CPUScore.setText(CPU.name + ": " + CPU.score);
+            }
+        });
+
+
+        /*
+        *  when the throw button is clicked it will
+        *  randomize the dice for both P1 and CPU at the start
+        */
         throwButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                P1.RollScoreReset();
+                CPU.RollScoreReset();
+              if(P1.rolls == 3 && CPU.rolls == 3){
+                // roll the dice for both players
                 diceRoller.roll(P1.dice, P1.DiceImages);
                 diceRoller.roll(CPU.dice, CPU.DiceImages);
+                // add 1 to the rounds counter and displays the counter again
+                rounds =rounds + 1;
+                roundsTxt.setText("round: " + rounds);
+                /*
+                * for later
+                *
+                * P1.rolls = P1.rolls - 1
+                * CPU.rolls = CPU.rolls - 1
+                *
+                * */
+
+                  P1.sumScores();
+                  playerRollScore.setText(P1.name + " roll score: " + P1.rollScore);
+
+
+                  CPU.sumScores();
+                  CPURollScore.setText(CPU.name + " roll score: " + CPU.rollScore);
+
+
+              }else if(P1.rolls != 0){
+                // re roll for player
+              }else if(CPU.rolls!= 0){
+                // re roll for CPU automatically with logic
+              }
             }
+
         });
+
+
+
+
 
     }
 
--- app/src/main/java/com/example/dicegame/Player.java
+++ app/src/main/java/com/example/dicegame/Player.java
@@ -3,9 +3,10 @@ package com.example.dicegame;
 import android.widget.ImageView;
 
 public class Player {
-    private final String name;
+     String name;
     int score = 0;
-    int rolls = 2;
+    int rollScore = 0;
+    int rolls = 3;
     ImageView[] DiceImages = new ImageView[5];
     int[] dice = new int[5];
     public Player(String PName){
@@ -16,6 +17,23 @@ public class Player {
     public void RollReset(){
 
         rolls = 2;
+
+    }
+
+    // reset score that will be used every roll and re roll to be counted up
+    public  void  RollScoreReset(){
+        rollScore = 0;
+
+    }
+    //sum of scores
+    public void sumScores(){
+        for(int die: dice){
+            rollScore = rollScore + die;
+        }
+    }
+    // add current roll scores to the overall score
+    public void  sumOverallScores(){
+        score = score + rollScore;
 
     }
 
